501768f0d6fc0c3587e4c7f67c930dc60cd2ac84
hestarium <hestarium@hotmail.com>
2019-05-31 15:16:55 +0900

    Push any other things

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.awt.*;
 import java.io.IOException;
+import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
@@ -10,6 +11,15 @@ public class Main {
         TimeThread timeThread = new TimeThread(watchSystem);
         timeThread.run();
 
+        /*
+        Calendar.YEAR // 1
+        Calendar.MONTH // 2
+        Calendar.DATE // 5
+        Calendar.HOUR_OF_DAY // 11
+        Calendar.MINUTE // 12
+        Calendar.SECOND // 13
+        Calendar.MILLISECOND // 14
+        */
 
     }
 }
